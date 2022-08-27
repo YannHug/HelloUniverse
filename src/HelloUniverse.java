225ccb7b5e0b604ff7fee00d7963583103a78861
Yannh <yannhweb@gmail.com>
2022-08-27 13:30:22 +0200

    Utiliser switch, case et default

--- src/HelloUniverse.java
+++ src/HelloUniverse.java
@@ -1,21 +1,15 @@
 public class HelloUniverse {
     public static void main(String... args) {
+        int nbPlanetes = 7;
 
-        String phraseIntroduction = "En %d, les planètes du système solaire étaient au nombre de : %d";
-        short annee = 2005;
-        int nbPlanete = 0;
-
-        if (annee >= 1600 && annee <= 2020) {
-            if (annee < 1700) {
-                nbPlanete = 7;
-            } else if (annee < 1800 || annee >= 2006) {
-                nbPlanete = 8;
-            } else {
-                nbPlanete = 9;
-            }
-            System.out.printf(phraseIntroduction, annee, nbPlanete);
-        } else {
-            System.out.println("Le programme ne peut pas fournir de résultat pour l'année " + annee);
+        switch (nbPlanetes) {
+            case 7 -> System.out.println("On sait qu'au 16ème siècle, seules 7 planètes avaient été découvertes");
+            case 8 ->
+                    System.out.println("On sait que le nombre de planètes est passé de 7 à 8 au 17ème siècle, mais il a également été réduit de 9 à 8 en 2006");
+            case 9 ->
+                    System.out.println("On sait que le nombre de planètes est passé de 8 à 9 au 18ème siècle et ce jusqu'en 2006, où ce nombre a été réduit à 8");
+            default -> System.out.println("Le programme ne peut pas fournir de résultat pour " + nbPlanetes);
         }
+
     }
 }
