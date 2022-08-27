fdef075b8a903eabb0d2aa3b9cd03a6a8eba1524
Yannh <yannhweb@gmail.com>
2022-08-27 13:22:55 +0200

    1er variable

--- src/HelloUniverse.java
+++ src/HelloUniverse.java
@@ -1,5 +1,8 @@
 public class HelloUniverse {
-    public static void main(String... arg){
-        System.out.println("Hello Universe!");
+    public static void main(String... args) {
+        int nombrePlanete = 8;
+        System.out.println("Aux dernières nouvelles, le nombre total de planètes dans le système solaire est de : " + nombrePlanete);
+        nombrePlanete = 9;
+        System.out.println("Il y a quelques années cependant, elles étaient au nombre de : " + nombrePlanete);
     }
 }
