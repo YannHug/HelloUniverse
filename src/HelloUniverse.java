1a8ba9ad60ee53550eb2b547139ad90651d5b097
Yannh <yannhweb@gmail.com>
2022-08-27 13:45:08 +0200

    Surcharge de méthode

--- src/HelloUniverse.java
+++ src/HelloUniverse.java
@@ -1,51 +1,43 @@
 public class HelloUniverse {
-    public static void main(String... args) {
 
+    public static void main(String... args) {
         Planete mercure = new Planete();
         mercure.nom = "Mercure";
         mercure.diametre = 4880;
-        mercure.matiere = "tellurique";
-
+        mercure.matiere = "Tellurique";
         Planete venus = new Planete();
         venus.nom = "Venus";
         venus.diametre = 12100;
-        venus.matiere = "tellurique";
-
+        venus.matiere = "Tellurique";
         Planete terre = new Planete();
         terre.nom = "Terre";
         terre.diametre = 12756;
-        terre.matiere = "tellurique";
-
+        terre.matiere = "Tellurique";
         Planete mars = new Planete();
         mars.nom = "Mars";
         mars.diametre = 6792;
-        mars.matiere = "tellurique";
-
+        mars.matiere = "Tellurique";
         Planete jupiter = new Planete();
         jupiter.nom = "Jupiter";
         jupiter.diametre = 142984;
-        jupiter.matiere = "gazeuse";
-
+        jupiter.matiere = "Gazeuse";
         Planete saturne = new Planete();
         saturne.nom = "Saturne";
         saturne.diametre = 120536;
-        saturne.matiere = "gazeuse";
-
+        saturne.matiere = "Gazeuse";
         Planete uranus = new Planete();
         uranus.nom = "Uranus";
         uranus.diametre = 51118;
-        uranus.matiere = "gazeuse";
-
+        uranus.matiere = "Gazeuse";
         Planete neptune = new Planete();
         neptune.nom = "Neptune";
         neptune.diametre = 49532;
-        neptune.matiere = "gazeuse";
+        neptune.matiere = "Gazeuse";
 
-        Planete planeteX = new Planete();
-
-        neptune.revolution(-3542);
-        mars.rotation(-684);
-        venus.rotation(1250);
+        mars.accueillirVaisseau(8);
+        mars.accueillirVaisseau("FREGATE");
+        System.out.println(" Le nombre d'humains ayant déjà séjourné sur Mars est actuellement de " + mars.totalVisiteurs);
 
     }
+
 }
--- src/Planete.java
+++ src/Planete.java
@@ -1,15 +1,32 @@
 public class Planete {
     String nom;
-    String matiere;
     long diametre;
+    String matiere;
+    int totalVisiteurs = 0;
 
-    void revolution(int degresRevolution) {
-        int nbTourRevolution = degresRevolution / 360;
-        System.out.printf("%s a effectué %d tours complets autour de son étoile.\n", nom, nbTourRevolution);
+    int revolution(int degres) {
+        System.out.println("Je suis la planète " + nom + " et je tourne autour de mon étoile de " + degres + " degrés.");
+        return degres / 360;
     }
 
-    void rotation(int degresRotation) {
-        int nbTourRotation = degresRotation / 360;
-        System.out.printf("%s a effectué %d tours complets autour de soi-même.\n", nom, nbTourRotation);
+    int rotation(int degres) {
+        System.out.println("Je suis la planète " + nom + " et je tourne sur moi-même de " + degres + " degrés.");
+        return degres / 360;
+    }
+
+    void accueillirVaisseau(int nouveauxHumains) {
+        totalVisiteurs += nouveauxHumains;
+    }
+
+    void accueillirVaisseau(String typeVaisseau) {
+        if (typeVaisseau == "CHASSEUR") {
+            totalVisiteurs += 3;
+        } else if (typeVaisseau == "FREGATE") {
+            totalVisiteurs += 12;
+        } else if (typeVaisseau == "CROISEUR") {
+            totalVisiteurs += 50;
+        } else {
+            System.out.println("Vaisseau Inconnu");
+        }
     }
 }
