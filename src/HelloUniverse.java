69cb95c79e0c0b676c8bade1a213fe8f83d01029
Yannh <yannhweb@gmail.com>
2022-08-27 13:27:18 +0200

    Utiliser if et else

--- src/HelloUniverse.java
+++ src/HelloUniverse.java
@@ -1,11 +1,14 @@
 public class HelloUniverse {
     public static void main(String... args) {
-        short anneeActuelle = 2002;
 
-        if (anneeActuelle < 2006) {
-            System.out.printf("En %d, les planètes du système solaire étaient au nombre de : 9", anneeActuelle);
-        } else {
-            System.out.printf("En %d, les planètes du système solaire étaient au nombre de : 8", anneeActuelle);
-        }
+        String phraseIntroduction = "En %d, les planètes du système solaire étaient au nombre de : %d";
+        short annee = 2005;
+        int nbPlanete = 0;
+
+        if (annee < 2006) {
+            nbPlanete = 9;
+        } else
+            nbPlanete = 8;
+        System.out.printf(phraseIntroduction, annee, nbPlanete);
     }
 }
