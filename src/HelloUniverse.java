f3f8f8f02e8dbc840163e99e2bc40154edab081f
Yannh <yannhweb@gmail.com>
2022-08-27 13:28:00 +0200

    Utiliser if, else if et else

--- src/HelloUniverse.java
+++ src/HelloUniverse.java
@@ -5,10 +5,17 @@ public class HelloUniverse {
         short annee = 2005;
         int nbPlanete = 0;
 
-        if (annee < 2006) {
-            nbPlanete = 9;
-        } else
-            nbPlanete = 8;
-        System.out.printf(phraseIntroduction, annee, nbPlanete);
+        if (annee >= 1600 && annee <= 2020) {
+            if (annee < 1700) {
+                nbPlanete = 7;
+            } else if (annee < 1800 || annee >= 2006) {
+                nbPlanete = 8;
+            } else {
+                nbPlanete = 9;
+            }
+            System.out.printf(phraseIntroduction, annee, nbPlanete);
+        } else {
+            System.out.println("Le programme ne peut pas fournir de résultat pour l'année " + annee);
+        }
     }
 }
