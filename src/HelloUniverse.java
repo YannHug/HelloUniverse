20f0b65402654738d95f1b792366c016d8956c36
Yannh <yannhweb@gmail.com>
2022-08-27 13:25:03 +0200

    Incrémenter le nombre de planètes

--- src/HelloUniverse.java
+++ src/HelloUniverse.java
@@ -1,10 +1,10 @@
 public class HelloUniverse {
     public static void main(String... args) {
         int nombrePlanete = 8;
-        String debutDePhrase = "Aux dernières nouvelles, le nombre total de planètes dans le système solaire est de : ";
-        System.out.println(debutDePhrase + nombrePlanete);
-        nombrePlanete = 9;
-        debutDePhrase = "Il y a quelques années cependant, elles étaient au nombre de : ";
-        System.out.println(debutDePhrase + nombrePlanete);
+        String phrase = "Aux dernières nouvelles, le nombre total de planètes dans le système solaire est de : ";
+        System.out.println(phrase + nombrePlanete);
+        nombrePlanete++;
+        phrase = "Il y a quelques années cependant, elles étaient au nombre de : ";
+        System.out.println(phrase + nombrePlanete);
     }
 }
