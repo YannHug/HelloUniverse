924f36d3ca8618aa571e776e6eb01a1e49761b5d
Yannh <yannhweb@gmail.com>
2022-08-27 13:26:38 +0200

    Première instruction conditionnelle

--- src/HelloUniverse.java
+++ src/HelloUniverse.java
@@ -1,10 +1,11 @@
 public class HelloUniverse {
     public static void main(String... args) {
-        int nombrePlanetes = 8;
-        String phrase = "Aux dernières nouvelles, le nombre total de planètes dans le système solaire est de : ";
-        System.out.println(phrase + nombrePlanetes);
-        nombrePlanetes++;
-        phrase = "Il y a quelques années cependant, elles étaient au nombre de : ";
-        System.out.println(phrase + nombrePlanetes);
+        short anneeActuelle = 2002;
+
+        if (anneeActuelle < 2006) {
+            System.out.printf("En %d, les planètes du système solaire étaient au nombre de : 9", anneeActuelle);
+        } else {
+            System.out.printf("En %d, les planètes du système solaire étaient au nombre de : 8", anneeActuelle);
+        }
     }
 }
